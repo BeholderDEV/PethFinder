4da05227f9f83b47897503bd2fa848fc38ad7a35
AlissonSteffens <alisson.steffens@hotmail.com>
2016-09-14 17:00:25 -0300

    lendo XML

---
+++ src/core/XMLReader.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import core.model.Mapa;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ *
+ * @author lite
+ */
+public class XMLReader {
+    
+    public static Mapa lerXML(){
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("Arquivo XML", "xml");
+        fileChooser.setFileFilter(extensionFilter);
+        File file = null;
+        int returnVal = fileChooser.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            file = fileChooser.getSelectedFile();
+            try {
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(file);
+                doc.getDocumentElement().normalize();                
+                NodeList nodes = doc.getElementsByTagName("LINHAS");
+                Node node = nodes.item(0);
+                int linhas = Integer.parseInt(node.getTextContent());
+                nodes = doc.getElementsByTagName("COLUNAS");
+                node = nodes.item(0);
+                int colunas = Integer.parseInt(node.getTextContent());
+                Dimension tamanho = new Dimension(colunas, linhas);
+                nodes = doc.getElementsByTagName("INICIAL");
+                node = nodes.item(0);
+                String ponto = node.getTextContent();
+                String[] pos = ponto.split(",");
+                Point inicial = new Point(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]));
+                nodes = doc.getElementsByTagName("FINAL");
+                node = nodes.item(0);
+                ponto = node.getTextContent();
+                pos = ponto.split(",");
+                Point fina = new Point(Integer.parseInt(pos[0]), Integer.parseInt(pos[1]));
+                
+                int[][] matriz = new int[linhas][colunas];
+                for (int i = 0; i < linhas; i++) {
+                    for (int j = 0; j < colunas; j++) {
+                        matriz[i][j]=0;
+                    }
+                }
+                
+                nodes = doc.getElementsByTagName("MURO");
+                for (int i = 0; i < nodes.getLength(); i++) {
+                    node = nodes.item(i);
+                    ponto = node.getTextContent();
+                    pos = ponto.split(",");
+                    matriz[Integer.parseInt(pos[0])][Integer.parseInt(pos[1])]=1;
+                }
+                
+                Mapa mapa = new Mapa(tamanho, matriz, inicial, fina);
+                return mapa;
+            } catch (Exception ex) {
+                System.err.println(ex);
+            }
+        }
+        return null;
+    }
+}
---
+++ src/core/model/Mapa.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core.model;
+
+import java.awt.Dimension;
+import java.awt.Point;
+
+/**
+ *
+ * @author lite
+ */
+public class Mapa {
+    private Dimension tamanho;
+    private int[][] matrix;
+    private Point pontoInicial;
+    private Point pontoFinal;
+
+    public Mapa(Dimension tamanho, int[][] matrix, Point pontoInicial, Point pontoFinal) {
+        this.tamanho = tamanho;
+        this.matrix = matrix;
+        this.pontoInicial = pontoInicial;
+        this.pontoFinal = pontoFinal;
+    }
+
+    
+    public Dimension getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(Dimension tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public Point getPontoInicial() {
+        return pontoInicial;
+    }
+
+    public void setPontoInicial(Point pontoInicial) {
+        this.pontoInicial = pontoInicial;
+    }
+
+    public Point getPontoFinal() {
+        return pontoFinal;
+    }
+
+    public void setPontoFinal(Point pontoFinal) {
+        this.pontoFinal = pontoFinal;
+    }
+
+    @Override
+    public String toString() {
+        String str="";
+        str = str.concat("\nTamanho: "+tamanho.toString());
+        str = str.concat("\nInicial: "+pontoInicial.toString());
+        str = str.concat("\nFinal: "+pontoFinal.toString()+"\n");
+        
+        for (int i = 0; i < tamanho.height; i++) {
+            for (int j = 0; j < tamanho.width; j++) {
+                str = str.concat(matrix[i][j]+" ");
+            }
+            str = str.concat("\n");
+        }
+        
+        return str;
+    }
+    
+}
